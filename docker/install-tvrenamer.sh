336c5ef31614ed310a966983438a4de1810acab2
Chris Osborn <fozztexx@fozztexx.com>
2019-10-10 12:25:17 -0700

    Added tvrenamer installer.

---
+++ docker/install-tvrenamer.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+BASE=$(basename $0)
+TMP=/tmp/${BASE}.$$
+
+mkdir ${TMP}
+cd ${TMP}
+git clone https://github.com/meermanr/TVSeriesRenamer
+cp TVSeriesRenamer/tvrenamer.pl /usr/local/bin
+
+cd
+rm -rf ${TMP}
+exit $STATUS

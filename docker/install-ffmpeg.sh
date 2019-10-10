0888499e1716ae1aee4f5f5c07f5e46c9e980177
Chris Osborn <fozztexx@fozztexx.com>
2019-10-10 14:19:35 -0700

    Fix installers to install latest version of handbrake.

--- docker/install-ffmpeg.sh
+++ docker/install-ffmpeg.sh
@@ -7,8 +7,15 @@ cd ${TMP}
 git clone /tmp/ffmpeg.bundle
 cd ffmpeg
 #  --enable-libfaac --enable-gnutls
-./configure --enable-gpl --enable-version3 --enable-nonfree --enable-libx264 --enable-libfreetype --enable-pic --enable-shared --enable-openssl && make && make install
-STATUS=$?
-cd ..
+./configure --enable-gpl --enable-version3 --enable-nonfree --enable-libx264 --enable-libfreetype --enable-pic --enable-shared --enable-openssl
+STATUS=$? ; if [ $STATUS != 0 ] ; then exit $STATUS ; fi
+
+make -j4
+STATUS=$? ; if [ $STATUS != 0 ] ; then exit $STATUS ; fi
+
+make install
+STATUS=$? ; if [ $STATUS != 0 ] ; then exit $STATUS ; fi
+
+cd
 rm -rf ${TMP}
 exit $STATUS
--- docker/install-handbrake.sh
+++ docker/install-handbrake.sh
@@ -6,10 +6,15 @@ mkdir ${TMP}
 cd ${TMP}
 #git clone https://github.com/HandBrake/HandBrake
 git clone /tmp/HandBrake.bundle
+STATUS=$? ; if [ $STATUS != 0 ] ; then exit $STATUS ; fi
+
 cd HandBrake
+#export PKG_CONFIG_PATH=/usr/lib/x86_64-linux-gnu/pkgconfig/
+
 echo Configuring
 ./configure --disable-gtk --launch && make --directory=build install
-STATUS=$?
-cd ..
+STATUS=$? ; if [ $STATUS != 0 ] ; then exit $STATUS ; fi
+
+cd
 rm -rf ${TMP}
 exit $STATUS

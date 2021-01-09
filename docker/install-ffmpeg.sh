6c94d01c733a6657d25fab1be26afd04a37ea7a3
Chris Osborn <fozztexx@fozztexx.com>
2021-01-09 14:43:29 -0800

    Include libmp3lame in ffmpeg.

--- docker/install-ffmpeg.sh
+++ docker/install-ffmpeg.sh
@@ -9,7 +9,7 @@ cd ${TMP}
 git clone /tmp/ffmpeg.bundle
 cd ffmpeg
 #  --enable-libfaac --enable-gnutls
-./configure --enable-gpl --enable-version3 --enable-nonfree --enable-libx264 --enable-libfreetype --enable-pic --enable-shared --enable-openssl
+./configure --enable-gpl --enable-version3 --enable-nonfree --enable-libx264 --enable-libfreetype --enable-pic --enable-shared --enable-openssl --enable-libmp3lame
 
 make -j4
 make install

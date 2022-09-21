cf15953e887719071c3b9caeaca5e825688b7216
Chris Osborn <fozztexx@fozztexx.com>
2022-09-21 08:29:36 -0700

    First version of autorip, designed to automatically rip and delete things from a specific directory.

--- docker/install-ffmpeg.sh
+++ docker/install-ffmpeg.sh
@@ -9,7 +9,17 @@ cd ${TMP}
 git clone /tmp/FFmpeg.bundle ffmpeg
 cd ffmpeg
 #  --enable-libfaac --enable-gnutls
-./configure --enable-gpl --enable-version3 --enable-nonfree --enable-libx264 --enable-libfreetype --enable-pic --enable-shared --enable-openssl --enable-libmp3lame
+./configure \
+    --enable-gpl \
+    --enable-libfreetype \
+    --enable-libmp3lame \
+    --enable-libx264 \
+    --enable-libx265 \
+    --enable-nonfree \
+    --enable-openssl \
+    --enable-pic \
+    --enable-shared \
+    --enable-version3 \
 
 make -j4
 make install

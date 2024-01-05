4a91bde50c1761f0ae32f798069e296330f54976
Chris Osborn <chris.osborn@intel.com>
2024-01-05 07:49:22 -0800

    Switch to using Docker multi-stage to reduce adding packages to final image that were only for compiling tools.

--- docker/install-ccextractor.sh
+++ docker/install-ccextractor.sh
@@ -12,7 +12,26 @@ git clone /tmp/ccextractor.bundle
 cd ccextractor/linux
 ./build -without-rust
 
-cp ccextractor /usr/bin
+grep -E '^[A-Z][A-Z_]+=' ../package_creators/debian.sh  > /tmp/values
+. /tmp/values
+
+PKG_ROOT="/tmp/${PROGRAM_NAME}"
+mkdir "${PKG_ROOT}"
+cd "${PKG_ROOT}"
+
+mkdir -p usr/bin
+cp ${TMP}/ccextractor/linux/ccextractor usr/bin
+
+mkdir DEBIAN
+echo Package: "${PROGRAM_NAME}" >> DEBIAN/control
+echo Version: "${VERSION}" >> DEBIAN/control
+echo Maintainer: "${MAINTAINER}" >> DEBIAN/control
+echo Architecture: "$(dpkg-architecture -q DEB_HOST_ARCH)" >> DEBIAN/control
+echo Depends: libtesseract4, libgpac11 >> DEBIAN/control
+echo Description: "${PROGRAM_NAME}" >> DEBIAN/control
+
+cd ..
+dpkg-deb --build "${PKG_ROOT}"
 
 cd
 rm -rf ${TMP}
